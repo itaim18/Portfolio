75f29137f89b3a145ed3d8f6c7f3e9605711f43f
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-01-10 15:00:04 +0200

    sounds updated

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -2,15 +2,20 @@ import { useContext } from "react";
 import styles from "./Button.module.css";
 import useSound from "use-sound";
 import on from "../../../sounds/switch-on.mp3";
+import off from "../../../sounds/switch-off.mp3";
 import soundContext from "../../../context/soundContext";
 const Button = (props) => {
   const soundOn = useContext(soundContext);
   const [play] = useSound(on, { volume: 0.5 });
+  const [play2] = useSound(off);
   const { colorFront, colorEdge } = props;
   return (
     <button
       onMouseEnter={() => {
         soundOn.soundOn ? play() : null;
+      }}
+      onClick={() => {
+        soundOn.soundOn ? play2() : null;
       }}
       style={{ color: "white", margin: "20px 5px" }}
       className={styles.buttonPushable}
